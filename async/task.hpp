8bc4b8585fb2513ab6f068e27ae1c937730d54db
jiaowenyao <jiaowenyao163@163.com>
2026-01-27 16:32:21 +0800

    fix: 修复Task destroy两次的bug

--- async/task.hpp
+++ async/task.hpp
@@ -1,7 +1,10 @@
 #include <coroutine>
 #include <exception>
-#include <format>
 #include <iostream>
+#include <optional>
+#include <assert.h>
+#include <format>
+#include "common/utils/noncopyable.h"
 
 
 namespace ynet::async {
@@ -19,7 +22,7 @@ struct TaskPromiseBase {
          * callee: 当前被挂起的协程
          * return: void或者下一个要恢复的协程句柄
          */
-        template<typename T>
+        template <typename T>
         std::coroutine_handle<> await_suspend(std::coroutine_handle<T> callee) const noexcept {
             /**
              * 如果有调用者，就恢复调用者
@@ -38,11 +41,11 @@ struct TaskPromiseBase {
                         std::cerr << std::format("catch a exception: {}", e.what());
                         std::terminate();
                     }
-                    // 销毁协程
-                    callee.destroy();
-                    // 返回空协程句柄
-                    return std::noop_coroutine();
                 }
+                // 这里不主动调用destroy，而是交给Task析构时调用
+                // callee.destroy();
+                // 返回空协程句柄
+                return std::noop_coroutine();
             }
         }
 
@@ -71,6 +74,165 @@ struct TaskPromiseBase {
     std::exception_ptr m_ex = nullptr;
 };
 
+template <typename T>
+class Task;
+
+
+template <typename T>
+struct TaskPromise final : public TaskPromiseBase {
+    Task<T> get_return_object() noexcept;
+
+    template <typename F>
+        requires std::is_convertible_v<F&&, T> && std::is_constructible_v<T, F&&>
+    void return_value(F&& value) {
+        m_value = std::forward<F>(value);
+    }
+
+    T& result() & {
+        if (m_ex != nullptr) [[unlikely]] {
+            std::rethrow_exception(m_ex);
+        }
+        assert(m_value.has_value());
+        return m_value.value();
+    }
+
+    T&& result() && {
+        if (m_ex != nullptr) [[unlikely]] {
+            std::rethrow_exception(m_ex);
+        }
+        assert(m_value.has_value());
+        return std::move(m_value.value());
+    }
+
+private:
+    std::optional<T> m_value = std::nullopt;
+};
+
+template <>
+struct TaskPromise<void> final : public TaskPromiseBase {
+    Task<void> get_return_object() noexcept;
+
+    constexpr void return_void() const noexcept {}
+
+    void result() const {
+        if (m_ex != nullptr) [[unlikely]] {
+            return std::rethrow_exception(m_ex);
+        }
+    }
+
+};
+
+
+template <typename T = void>
+class [[nodiscard]] Task : ynet::utils::Noncopyable {
+public:
+    using promise_type = TaskPromise<T>;
+private:
+    struct AwaitableBase {
+        // 被等待的对象
+        std::coroutine_handle<promise_type> m_callee;
+        AwaitableBase(std::coroutine_handle<promise_type> callee) noexcept
+            : m_callee(callee) {}
+
+        // 如果已经完成，就不需要等待了
+        bool await_ready() const noexcept {
+            return !m_callee || m_callee.done();
+        }
+
+        // 记录调用者，并跳转到被等待的协程
+        template <typename PromiseType>
+        std::coroutine_handle<> await_suspend(std::coroutine_handle<PromiseType> caller) {
+            m_callee.promise().m_caller = caller;
+            return m_callee;
+        }
+    };
+public:
+    Task() noexcept = default;
+
+    explicit Task(std::coroutine_handle<promise_type> handle)
+        : m_handle(handle) {}
+
+    ~Task() {
+        if (m_handle) {
+            m_handle.destroy();
+        }
+    }
+
+    Task(Task&& other) noexcept
+        : m_handle(std::move(other.m_handle)) {
+        other.m_handle = nullptr;
+    }
+
+    Task& operator=(Task&& other) noexcept {
+        if (std::addressof(other) != this) [[likely]] {
+            if (m_handle) {
+                m_handle.destroy();
+            }
+            m_handle = std::move(other.m_handle);
+            other.m_handle = nullptr;
+        }
+        return *this;
+    }
+
+    auto operator co_await() const & noexcept {
+        struct Awaitable final : public AwaitableBase {
+            decltype(auto) await_resume() {
+                if (!this->m_callee) [[unlikely]] {
+                    throw std::logic_error("m_handle is nullptr");
+                }
+                return this->m_callee.promise().result();
+            }
+        };
+        return Awaitable(m_handle);
+    }
+
+    auto operator co_await() const && noexcept {
+        struct Awaitable final : public AwaitableBase {
+            decltype(auto) await_resume() {
+                if (!this->m_callee) [[unlikely]] {
+                    throw std::logic_error("m_handle is nullptr");
+                }
+                if constexpr (std::is_same_v<T, void>) {
+                    return this->m_callee.promise().result();
+                }
+                else {
+                    return std::move(this->m_callee.promise().result());
+                }
+            }
+        };
+        return Awaitable(m_handle);
+    }
+
+    std::coroutine_handle<promise_type> task() {
+        if (m_handle == nullptr) [[unlikely]] {
+            throw std::logic_error("m_handle is nullptr");
+        }
+        auto res = std::move(m_handle);
+        m_handle = nullptr;
+        return res;
+    }
+
+    std::coroutine_handle<promise_type> handle() {
+        return m_handle;
+    }
+
+    void resume() const {
+        m_handle.resume();
+    }
+
+private:
+    std::coroutine_handle<promise_type> m_handle = nullptr;
+};
+
+
+template <typename T>
+inline Task<T> TaskPromise<T>::get_return_object() noexcept {
+    return Task<T>(std::coroutine_handle<TaskPromise>::from_promise(*this));
+}
+
+inline Task<void> TaskPromise<void>::get_return_object() noexcept {
+    return Task<void>(std::coroutine_handle<TaskPromise>::from_promise(*this));
+}
 
 
 } // namesapce ynet::async
--- test/init.cpp
+++ test/init.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+#include "async/task.hpp"
+
+using ynet::async::Task;
+
+Task<int> get_num() {
+    co_return 42;
+}
+
+Task<void> message() {
+    std::cout << "message" << std::endl;
+
+    int num = co_await get_num();
+
+    std::cout << "num=" << num << std::endl;
+}
+
+Task<void> process() {
+    co_await message();
+}
+
 
 int main() {
+    std::cout << "hello world" << std::endl;
+
+    Task<void> process_task = process();
+
+    process_task.handle().resume();
 
     return 0;
 }
